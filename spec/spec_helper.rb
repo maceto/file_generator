3e8539c91da61e6b102c0d5621cc66446c950e1e
Martin Aceto <maceto@backing-online.com>
2012-02-18 18:04:24 -0300

    add simplocov for test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
-require 'file_generator'
 require 'rspec'
+require 'simplecov'
+
+SimpleCov.start
+
+require 'file_generator'
 
 Dir[File.join(Dir.pwd, "spec/support/**/*.rb")].each {|f| require f}
 
